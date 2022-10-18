b1025e79b73513c098b2c75437f09d3a8551f9d1
Dyes Junior Sakwa <dyes.junior@student.moringaschool.com>
2022-10-18 19:32:40 +0300

    Done

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,19 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailAddressParser
+    attr_accessor :parse
+    def initialize(parse)
+        @parse=parse
+    end
+
+    def parse
+        @parse.split(/,?\s/).uniq
+    end
+end
+
+#uniq is to see there are no duplicates
+#A space delimited email means these are emails that are separated by a
+#a literal empty space instead of a comma and it is for that...
+#...reason that we will need the regex
